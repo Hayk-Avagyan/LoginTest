e1f715183f6ac89463725559d4b5e69a09d94931
Hayk Avagyan <h.avagyan92@gmail.com>
2017-09-19 17:09:46 +0400

    save login info at shared preferences

--- app/src/main/java/com/hayk/myapplication/conttrollers/LoginInfoController.java
+++ app/src/main/java/com/hayk/myapplication/conttrollers/LoginInfoController.java
@@ -8,37 +8,45 @@ import android.util.Log;
 import com.hayk.myapplication.model.LoginInfo;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by User on 13.09.2017.
  */
 public class LoginInfoController {
+
     private static LoginInfoController instance;
-    private ArrayList<LoginInfo> registeredData;
-    private static final String USER_PREFERENCES_KEY = "user-preferences_key";
-    private static final String CURRENT_TIME_PREFERENCES_KEY = "current_time_preferences_key";
 
     public ArrayList<LoginInfo> getRegisteredData(Context context) {
+
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        registeredData.add(new LoginInfo(sharedPreferences.getString(USER_PREFERENCES_KEY, ""), sharedPreferences.getString(CURRENT_TIME_PREFERENCES_KEY, "")));
-        return registeredData;
+        ArrayList<LoginInfo> infoArrayList = new ArrayList<>();
+
+        Map<String, ?> allEntries = sharedPreferences.getAll();
+
+        for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+
+            String loginInfoJsonString = (String) entry.getValue();
+            infoArrayList.add(LoginInfo.convertFromJson(loginInfoJsonString));
+        }
+        return infoArrayList;
     }
 
     public void addRegisteredData(LoginInfo loginInfo, Context context) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(USER_PREFERENCES_KEY, loginInfo.getRegisteredEmail());
-        editor.putString(CURRENT_TIME_PREFERENCES_KEY, loginInfo.getRegistrationTime());
+
+        String newKey = String.valueOf(sharedPreferences.getAll().size() + 1);
+
+        editor.putString(newKey, loginInfo.convertToJson(loginInfo));
         editor.apply();
-        registeredData.add(loginInfo);
     }
 
     private LoginInfoController() {
-        this.registeredData = new ArrayList<>();
     }
 
     public static LoginInfoController getInstance() {
-        Log.w("Tag", "ItemsContainer:getInstance()");
+        Log.w("Tag", "LoginInfoController:getInstance()");
         if (null == instance) {
             instance = new LoginInfoController();
         }
--- app/src/main/java/com/hayk/myapplication/model/LoginInfo.java
+++ app/src/main/java/com/hayk/myapplication/model/LoginInfo.java
@@ -1,5 +1,7 @@
 package com.hayk.myapplication.model;
 
+import com.google.gson.Gson;
+
 /**
  * Created by User on 14.09.2017.
  */
@@ -28,5 +30,15 @@ public class LoginInfo {
 
     public void setRegistrationTime(String registrationTime) {
         this.registrationTime = registrationTime;
+    }
+
+    public static LoginInfo convertFromJson(String s) {
+        Gson gson = new Gson();
+        return gson.fromJson(s, LoginInfo.class);
+    }
+
+    public String convertToJson(LoginInfo loginInfo) {
+        Gson gson = new Gson();
+        return gson.toJson(loginInfo);
     }
 }
